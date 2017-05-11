a10c88a17739fdb16cf2eb7d53f706a306182b8d
fluxtheory <fluxtheory@hotmail.com>
2017-05-11 15:21:40 +1000

    Update Grid.java

--- src/Grid.java
+++ src/Grid.java
@@ -13,50 +13,83 @@ import javax.swing.JPanel;
 	
 public class Grid extends JPanel{
 
-	private static int sml_side = 3; 
+	
 	// add additional presets for medium, large maps
 	private static Color bkgrd = Color.black;
-	private JLabel[][] fieldGrid = new JLabel[9][9];  
+	private JPanel[][] panelArray = new JPanel[16][16]; 
 
-	public Grid(){
+	public Grid(int size){
 	
-		JPanel mainPanel = new JPanel(new GridLayout(3,3));   //manager divides up space in 3 rows and 3 columns.
-		//mainPanel.setBorder(BorderFactory.createEmptyBorder(gap,gap,gap,gap));
-		JPanel[][] smlPanels = new JPanel[3][3];   //creates the panels to populate layout.
-	
-		for (int y = 0; y<smlPanels.length; y++){
+		JPanel mainPanel = new JPanel(new GridLayout(16,16));   //manager divides up space in 3 rows and 3 columns.
+	 
 		
-			for (int x = 0; x <smlPanels.length; x++){
+		for (int y = 0; y< 16; y++){
+		
+			for (int x = 0; x < 16; x++){
+		
+				panelArray[y][x] = new JPanel(); //creates the panels to populate layout.
+				panelArray[y][x].setBackground(bkgrd);
+				//panelArray[y][x].setBorder(BorderFactory.createLineBorder(Color.WHITE));
+				mainPanel.add(panelArray[y][x]); 
+				
+			}
+		}
+	
+		if(size == 1){
 			
-				smlPanels[y][x] = new JPanel(new GridLayout(3,3, 0,0)); //subdivides larger gridlayout into smaller 3x3 layouts.
-				smlPanels[y][x].setBackground(bkgrd);
-				mainPanel.add(smlPanels[y][x]); 
-			}
-		}
+			 createSmallGrid(panelArray);
+			 
+		} else if (size == 2){
 	
-		for (int y = 0; y < fieldGrid.length; y++){
+			createMediumGrid(panelArray);
+			
+		} else if (size == 4){
+
+			createLargeGrid(panelArray);
+		}
 		
-			for (int x = 0; x < fieldGrid.length; x++){
-				fieldGrid[y][x] = createField();
-				int k = y/3;
-				int l = x/3;
-				smlPanels[k][l].add(fieldGrid[y][x]);
-			}
-		}
-	
+		
 		setLayout(new BorderLayout());
 		add(mainPanel,BorderLayout.CENTER);
 	
 	}
 	
-	public JLabel createField(){
+	public void createSmallGrid(JPanel[][] arr){   //should small resize? ideally yes. or we find some way of centering it.
+		//only activates a small subset of the grids for use and populate with floor and walls sprite.
+		//5x5 working space.  too small, that only leaves what, 9 grids for boxes, player And WALLS.
+		//6x6 in reality.
+		//activated grids turn green.
 		
-		JLabel label = new JLabel();
-		//label.setHorizontalAlignment(1);
-		label.setBorder(BorderFactory.createLineBorder(Color.WHITE));
-		//label.setBackground(Color.WHITE);
-		return label;
+		for (int y = 5; y <= 10; y++){
+			for (int x = 5; x <= 10; x++){
+				arr[y][x].setBackground(new Color(13,86,75));;
+				arr[y][x].setBorder(BorderFactory.createLineBorder(Color.WHITE));
+			}
+		}
 	}
 	
+	public void createMediumGrid(JPanel[][] arr){	
+		//activates a greater portion
+		//10x10
+		for (int y = 3; y < 13; y++){
+			for (int x = 3; x < 13; x++){
+				arr[y][x].setBackground(new Color(13,86,75));;
+				arr[y][x].setBorder(BorderFactory.createLineBorder(Color.WHITE));
+			}
+		}
+		
+	}
+	
+	public void createLargeGrid(JPanel[][] arr){
+		//16x16
+		for (int y = 0; y < 16; y++){
+			for (int x = 0; x < 16; x++){
+				arr[y][x].setBackground(new Color(13,86,75));;
+				arr[y][x].setBorder(BorderFactory.createLineBorder(Color.WHITE));
+			}
+		}
+	}
+	
+	
+	
 }
-
