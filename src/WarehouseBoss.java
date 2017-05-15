e8de684e0cef8af60179eb68ad86c5183dc3749a
EliGe <eligechang@gmail.com>
2017-05-15 18:14:03 +1000

    using warehouseboss to implement puzzle

--- src/WarehouseBoss.java
+++ src/WarehouseBoss.java
@@ -6,34 +6,19 @@
 
 public class WarehouseBoss {
 	FrontEnd fe;
-	BackEnd be;
 
 	public WarehouseBoss(){
 		fe = new FrontEnd();
-		be = new BackEnd();
+		fe.setVisible(true);
 	}
 
-	public static void main(String argv[]){
+	public static void main(String[] args){
 		WarehouseBoss wb = new WarehouseBoss();
-		wb.getBe().loadLevel(2);
-		wb.getBe().printLevel();
+		
 	}
 
-	public FrontEnd getFe() {
-		return fe;
-	}
 
-	public void setFe(FrontEnd fe) {
-		this.fe = fe;
-	}
 
-	public BackEnd getBe() {
-		return be;
-	}
 
-	public void setBe(BackEnd be) {
-		this.be = be;
-	}
-	
 	
 }
