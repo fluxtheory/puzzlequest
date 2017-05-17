0f5e67a70b5d6728383832a290991993b44d4790
AndyDung <tonypeteandyuen@gmail.com>
2017-05-17 16:24:08 +1000

    Update Grid.java
    Just add "size" attribute to the class

--- src/Grid.java
+++ src/Grid.java
@@ -11,11 +11,13 @@ public class Grid extends JPanel{
 
 	
 	LevelManager lm = new LevelManager();
-	GameGrid gg = lm.loadLevel(2);
+	GameGrid gg;
 	Image[] myImage = new Image[10];
+	private int size;
 	
-	
-	public Grid(int size){
+	public Grid(int size, int level){
+		this.size = size;
+		gg = lm.loadLevel(level);
 		setBounds(15,50,600,600);
 		setBackground(Color.WHITE);
 		for(int i =0; i < 10; i++){
@@ -25,8 +27,8 @@ public class Grid extends JPanel{
 	}
 	
 	public void paint(Graphics g){
-		for(int i = 0; i < 20; i++){
-			for(int j = 0; j < 20; j++){
+		for(int i = 0; i < this.size; i++){
+			for(int j = 0; j < this.size; j++){
 				g.drawImage(myImage[gg.getGrid().get(i).get(j)], j*30, i*30, null);
 			}
 		}
