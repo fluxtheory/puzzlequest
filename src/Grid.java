607952858507852e2bcb87e7ca563e1424a34a86
EliGe <eligechang@gmail.com>
2017-05-17 21:08:46 +1000

    basic work version

--- src/Grid.java
+++ src/Grid.java
@@ -2,23 +2,27 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
 	
-public class Grid extends JPanel{
+public class Grid extends JPanel implements KeyListener{
 
 	
 	LevelManager lm = new LevelManager();
-	GameGrid gg;
+	GameGrid gg = lm.loadLevel(2);
 	Image[] myImage = new Image[10];
-	private int size;
+	PlayerController pl = new PlayerController(gg);
+	
 	
 	public Grid(int size, int level){
-		this.size = size;
-		gg = lm.loadLevel(level);
-		setBounds(15,50,600,600);
+		setBounds(0,0,600,600);
+		addKeyListener(this);
 		setBackground(Color.WHITE);
 		for(int i =0; i < 10; i++){
 			myImage[i] = Toolkit.getDefaultToolkit().getImage("pic/"+ i + ".gif");
@@ -27,13 +31,57 @@ public class Grid extends JPanel{
 	}
 	
 	public void paint(Graphics g){
-		for(int i = 0; i < this.size; i++){
-			for(int j = 0; j < this.size; j++){
-				g.drawImage(myImage[gg.getGrid().get(i).get(j)], j*30, i*30, null);
+		for(int i = 0; i < 20; i++){
+			for(int j = 0; j < 20; j++){
+				g.drawImage(myImage[gg.getGrid().get(j).get(i)], i*30, j*30, this);
 			}
 		}
 	}
-	
-	
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("flag1");
+		if(e.getKeyCode()==KeyEvent.VK_UP){
+			pl.moveUp(gg);
+			repaint();
+		}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN){
+			pl.moveDown(gg);
+			repaint();
+		}
+		if(e.getKeyCode()==KeyEvent.VK_LEFT){
+			pl.moveLeft(gg);
+			repaint();
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+			pl.moveRight(gg);
+			gg.printGrid();
+			repaint();
+		}
+		if(gg.iswin()){
+			String msg = "Congratulations, you passed level " + "2" + "!!!";
+			int type = JOptionPane.YES_NO_OPTION;
+			String title = "Pass";
+			int choice = 0;
+			choice = JOptionPane.showConfirmDialog(null, msg, title, type);
+			if(choice == 1) System.exit(0);
+			else if(choice == 0){
+				//
+			}
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	
 }
