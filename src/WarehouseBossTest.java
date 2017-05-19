1782e470b3983fee41051668073af41efdbbfa34
Khiong Yang Chua <z5031761>
2017-05-19 13:07:07 +1000

    levelManager updated
    levelManager updated:
    -createLevel: creates a level from a wbl file from the levels folder
    -loadGame: loads a game from a save file from saves folder.
    -saveGame: saves a game into a save file in saves folder.

--- src/WarehouseBossTest.java
+++ src/WarehouseBossTest.java
@@ -1,16 +1,20 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class WarehouseBossTest {
 
 	@Test
-	public void test() {
+	public void test() throws IOException {
 		
 		WarehouseBoss wb = new WarehouseBoss();
-		wb.getBe().loadEmptyLevel();
-		wb.getBe().printLevel();
-		wb.getFe().RenderGrid(wb.getBe().getGg());
+		LevelManager lm = new LevelManager();
+		GameGrid gg = lm.loadGame(0);
+		
+		gg.printGrid();
+		
 		
 		// fail("Not yet implemented");
 	}
