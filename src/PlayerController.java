48b9ab542f5bac1d80db43d350abb5158d1125b8
EliGe <eligechang@gmail.com>
2017-05-22 18:48:45 +1000

    version can work

--- src/PlayerController.java
+++ src/PlayerController.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Player Controller class, used to control the player in the grid
@@ -6,144 +7,412 @@ import java.util.ArrayList;
  *
  */
 public class PlayerController {
-	private int manRow;
+	private int manRaw;
 	private int manCol;
+	GameGrid ggtemp;
 	private int[] player = new int[2];
+	Stack<Integer> mystack = new Stack<Integer>();
 	
-	public PlayerController(GameGrid gg){
+	public PlayerController(GameGrid gg, GameGrid ggtemp){
+		this.ggtemp = ggtemp;
 		this.player = gg.getPlayerCoordinate();
-		this.manRow = player[0];
+		this.manRaw = player[0];
 		this.manCol = player[1];
 	}
 	
 	public boolean iswin(GameGrid gg){
-		for(int i = 1; i < gg.getRowCount() + 1; i++){
-			for(int j = 1; j < gg.getColCount() + 1; j++){
-				if(gg.getCoordinateValue(i, j)==4 && gg.getCoordinateValue(i, j)!=9){
+		for(int i=0;i<gg.getRowCount();i++){
+			for(int j=0;j<gg.getColCount();j++){
+				if(ggtemp.getCoordinateValue(i, j)==4 && gg.getCoordinateValue(i, j)!=9){
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-
+	
 	public void moveRight(GameGrid gg){
-		if(gg.getCoordinateValue(manRow, manCol + 1) == gg.GROUND){
-			gg.SetCoordinateValue(manRow, manCol + 1, gg.PLAYER_RIGHT);
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			manCol++;
-		}
-
-		else if(gg.getCoordinateValue(manRow, manCol + 1) == gg.BOX){
-
-			if(gg.getCoordinateValue(manRow, manCol + 2) == gg.GROUND){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow, manCol + 2, gg.BOX);
-				gg.SetCoordinateValue(manRow, manCol + 1, gg.PLAYER_RIGHT);
+		if(gg.getCoordinateValue(manRaw, manCol + 1) == 2 || 
+			gg.getCoordinateValue(manRaw, manCol + 1) == 4){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
 			}
-			
-			else if(gg.getCoordinateValue(manRow, manCol + 2) == gg.OBJECTIVE){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow, manCol + 2, gg.OBJECTIVE_BOX);
-				gg.SetCoordinateValue(manRow, manCol + 1, gg.PLAYER_RIGHT);
+			gg.SetCoordinateValue(manRaw, manCol + 1, 7);
+			manCol++;
+			mystack.push(40);
+		}else if(gg.getCoordinateValue(manRaw, manCol + 1) == 3){
+			if(gg.getCoordinateValue(manRaw, manCol + 2) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw, manCol + 1, 7);
+				gg.SetCoordinateValue(manRaw, manCol + 2, 9);
+				manCol++;
+				mystack.push(41);
+			}else if(gg.getCoordinateValue(manRaw, manCol + 2) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+						ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+						gg.SetCoordinateValue(manRaw, manCol, 4);
+					}else{
+						gg.SetCoordinateValue(manRaw, manCol, 2);
+					}
+					gg.SetCoordinateValue(manRaw, manCol + 1, 7);
+					gg.SetCoordinateValue(manRaw, manCol + 2, 3);
+					manCol++;
+					mystack.push(41);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 7);
 			}
-			manCol++;
-		}
-		
-		else if(gg.getCoordinateValue(manRow, manCol + 1) == gg.OBJECTIVE){
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			gg.SetCoordinateValue(manRow, manCol + 1, gg.OBJECTIVE_PLAYER_RIGHT);
-			manCol++;
+		}else if(gg.getCoordinateValue(manRaw, manCol + 1) == 9){
+			if(gg.getCoordinateValue(manRaw, manCol + 2) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+					gg.SetCoordinateValue(manRaw, manCol + 1, 7);
+					gg.SetCoordinateValue(manRaw, manCol + 2, 9);
+					manCol++;
+					mystack.push(41);
+			}else if(gg.getCoordinateValue(manRaw, manCol + 2) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+						ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+						gg.SetCoordinateValue(manRaw, manCol, 4);
+					}else{
+						gg.SetCoordinateValue(manRaw, manCol, 2);
+					}
+					gg.SetCoordinateValue(manRaw, manCol + 1, 7);
+					gg.SetCoordinateValue(manRaw, manCol + 2, 3);
+					manCol++;
+					mystack.push(41);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 7);
+			}
+		}else if(gg.getCoordinateValue(manRaw, manCol + 1) == 1){
+			gg.SetCoordinateValue(manRaw, manCol, 7);
 		}
 	}
 	
 	public void moveLeft(GameGrid gg){
-		if(gg.getCoordinateValue(manRow, manCol - 1) == gg.GROUND){
-			gg.SetCoordinateValue(manRow, manCol - 1, gg.PLAYER_LEFT);
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			manCol--;
-		}
-
-		else if(gg.getCoordinateValue(manRow, manCol - 1) == gg.BOX){
-
-			if(gg.getCoordinateValue(manRow, manCol - 2) == gg.GROUND){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow, manCol - 2, gg.BOX);
-				gg.SetCoordinateValue(manRow, manCol - 1, gg.PLAYER_LEFT);
+		if(gg.getCoordinateValue(manRaw, manCol - 1) == 2 || 
+			gg.getCoordinateValue(manRaw, manCol - 1) == 4){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
 			}
-			
-			else if(gg.getCoordinateValue(manRow, manCol - 2) == gg.OBJECTIVE){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow, manCol - 2, gg.OBJECTIVE_BOX);
-				gg.SetCoordinateValue(manRow, manCol - 1, gg.PLAYER_LEFT);
+			gg.SetCoordinateValue(manRaw, manCol - 1, 6);
+			manCol--;
+			mystack.push(30);
+		}else if(gg.getCoordinateValue(manRaw, manCol - 1) == 3){
+			if(gg.getCoordinateValue(manRaw, manCol - 2) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw, manCol - 1, 6);
+				gg.SetCoordinateValue(manRaw, manCol - 2, 9);
+				manCol--;
+				mystack.push(31);
+			}else if(gg.getCoordinateValue(manRaw, manCol - 2) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+					}else{
+						gg.SetCoordinateValue(manRaw, manCol, 2);
+					}
+					gg.SetCoordinateValue(manRaw, manCol - 1, 6);
+					gg.SetCoordinateValue(manRaw, manCol - 2, 3);
+					manCol--;
+					mystack.push(31);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 6);
 			}
-			manCol--;
-		}
-		
-		else if(gg.getCoordinateValue(manRow, manCol - 1) == gg.OBJECTIVE){
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			gg.SetCoordinateValue(manRow, manCol - 1, gg.OBJECTIVE_PLAYER_LEFT);
-			manCol--;
+		}else if(gg.getCoordinateValue(manRaw, manCol - 1) == 9){
+			if(gg.getCoordinateValue(manRaw, manCol - 2) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+					gg.SetCoordinateValue(manRaw, manCol - 1, 6);
+					gg.SetCoordinateValue(manRaw, manCol - 2, 9);
+					manCol--;
+					mystack.push(31);
+			}else if(gg.getCoordinateValue(manRaw, manCol - 2) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+						ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+						gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw, manCol - 1, 6);
+				gg.SetCoordinateValue(manRaw, manCol - 2, 3);
+				manCol--;
+				mystack.push(31);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 6);
+				}
+		}else if(gg.getCoordinateValue(manRaw, manCol - 1) == 1){
+			gg.SetCoordinateValue(manRaw, manCol, 6);
 		}
 	}
 	
 	public void moveUp(GameGrid gg){
-		if(gg.getCoordinateValue(manRow - 1, manCol) == gg.GROUND){
-			gg.SetCoordinateValue(manRow - 1, manCol, gg.PLAYER_UP);
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			manRow--;
-		}
-
-		else if(gg.getCoordinateValue(manRow - 1, manCol) == gg.BOX){
-
-			if(gg.getCoordinateValue(manRow - 2, manCol) == gg.GROUND){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow - 2, manCol, gg.BOX);
-				gg.SetCoordinateValue(manRow - 1, manCol, gg.PLAYER_UP);
+		if(gg.getCoordinateValue(manRaw - 1, manCol) == 2 || 
+			gg.getCoordinateValue(manRaw - 1, manCol) == 4){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
 			}
-			
-			else if(gg.getCoordinateValue(manRow - 2, manCol) == gg.OBJECTIVE){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow - 2, manCol, gg.OBJECTIVE_BOX);
-				gg.SetCoordinateValue(manRow - 1, manCol, gg.PLAYER_UP);
+			gg.SetCoordinateValue(manRaw - 1, manCol, 8);
+			manRaw--;
+			mystack.push(10);
+		}else if(gg.getCoordinateValue(manRaw - 1, manCol) == 3){
+			if(gg.getCoordinateValue(manRaw - 2, manCol) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw - 1, manCol, 8);
+				gg.SetCoordinateValue(manRaw - 2, manCol, 9);
+				manRaw--;
+				mystack.push(11);
+			}else if(gg.getCoordinateValue(manRaw - 2, manCol) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+					}else{
+						gg.SetCoordinateValue(manRaw, manCol, 2);
+					}
+					gg.SetCoordinateValue(manRaw - 1, manCol, 8);
+					gg.SetCoordinateValue(manRaw - 2, manCol, 3);
+					manRaw--;
+					mystack.push(11);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 8);
 			}
-			manRow--;
-		}
-		
-		else if(gg.getCoordinateValue(manRow - 1, manCol) == gg.OBJECTIVE){
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			gg.SetCoordinateValue(manRow - 1, manCol, gg.OBJECTIVE_PLAYER_UP);
-			manRow--;
+		}else if(gg.getCoordinateValue(manRaw - 1, manCol) == 9){
+			if(gg.getCoordinateValue(manRaw - 2, manCol) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+					gg.SetCoordinateValue(manRaw - 1, manCol, 8);
+					gg.SetCoordinateValue(manRaw - 2, manCol, 9);
+					manRaw--;
+					mystack.push(11);
+			}else if(gg.getCoordinateValue(manRaw - 2, manCol) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+						ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+						gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw - 1, manCol, 8);
+				gg.SetCoordinateValue(manRaw - 2, manCol, 3);
+				manRaw--;
+				mystack.push(11);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 8);
+				}
+		}else if(gg.getCoordinateValue(manRaw - 1, manCol) == 1){
+			gg.SetCoordinateValue(manRaw, manCol, 8);
 		}
 	}
 	
 	public void moveDown(GameGrid gg){
-		if(gg.getCoordinateValue(manRow + 1, manCol) == gg.GROUND){
-			gg.SetCoordinateValue(manRow + 1, manCol, gg.PLAYER_DOWN);
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			manRow++;
-		}
-
-		else if(gg.getCoordinateValue(manRow + 1, manCol) == gg.BOX){
-
-			if(gg.getCoordinateValue(manRow + 2, manCol) == gg.GROUND){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow + 2, manCol, gg.BOX);
-				gg.SetCoordinateValue(manRow + 1, manCol, gg.PLAYER_DOWN);
+		if(gg.getCoordinateValue(manRaw + 1, manCol) == 2 || 
+			gg.getCoordinateValue(manRaw + 1, manCol) == 4){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
 			}
-			
-			else if(gg.getCoordinateValue(manRow + 2, manCol) == gg.OBJECTIVE){
-				gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-				gg.SetCoordinateValue(manRow + 2, manCol, gg.OBJECTIVE_BOX);
-				gg.SetCoordinateValue(manRow + 1, manCol, gg.PLAYER_DOWN);
+			gg.SetCoordinateValue(manRaw + 1, manCol, 5);
+			manRaw++;
+			mystack.push(20);
+		}else if(gg.getCoordinateValue(manRaw + 1, manCol) == 3){
+			if(gg.getCoordinateValue(manRaw + 2, manCol) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw + 1, manCol, 5);
+				gg.SetCoordinateValue(manRaw + 2, manCol, 9);
+				manRaw++;
+				mystack.push(21);
+			}else if(gg.getCoordinateValue(manRaw + 2, manCol) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+					}else{
+						gg.SetCoordinateValue(manRaw, manCol, 2);
+					}
+					gg.SetCoordinateValue(manRaw + 1, manCol, 5);
+					gg.SetCoordinateValue(manRaw + 2, manCol, 3);
+					manRaw++;
+					mystack.push(21);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 5);
 			}
-			manRow++;
-		}
-		
-		else if(gg.getCoordinateValue(manRow + 1, manCol) == gg.OBJECTIVE){
-			gg.SetCoordinateValue(manRow, manCol, gg.GROUND);
-			gg.SetCoordinateValue(manRow + 1, manCol, gg.OBJECTIVE_PLAYER_DOWN);
-			manRow++;
+		}else if(gg.getCoordinateValue(manRaw + 1, manCol) == 9){
+			if(gg.getCoordinateValue(manRaw + 2, manCol) == 4){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+					ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+					gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+					gg.SetCoordinateValue(manRaw + 1, manCol, 5);
+					gg.SetCoordinateValue(manRaw + 2, manCol, 9);
+					manRaw++;
+					mystack.push(21);
+			}else if(gg.getCoordinateValue(manRaw + 2, manCol) == 2){
+				if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+						ggtemp.getCoordinateValue(manRaw, manCol) == 9 ){
+						gg.SetCoordinateValue(manRaw, manCol, 4);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 2);
+				}
+				gg.SetCoordinateValue(manRaw + 1, manCol, 5);
+				gg.SetCoordinateValue(manRaw + 2, manCol, 3);
+				manRaw++;
+				mystack.push(21);
+				}else{
+					gg.SetCoordinateValue(manRaw, manCol, 5);
+				}
+		}else if(gg.getCoordinateValue(manRaw + 1, manCol) == 1){
+			gg.SetCoordinateValue(manRaw, manCol, 5);
 		}
 	}
+	public void backup(int t, GameGrid gg){
+		if(t == 10){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+			}
+		}else if(t == 11){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 9);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 3);
+			}
+			if(ggtemp.getCoordinateValue(manRaw - 1 , manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw - 1 , manCol) == 9){
+				gg.SetCoordinateValue(manRaw - 1, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw - 1, manCol, 2);
+			}
+		}
+		gg.SetCoordinateValue(manRaw + 1, manCol, 8);
+		manRaw++;
+	}
+	public void backdown(int t, GameGrid gg){
+		if(t == 20){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+			}
+		}else if(t == 21){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 9);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 3);
+			}
+			if(ggtemp.getCoordinateValue(manRaw + 1 , manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw + 1 , manCol) == 9){
+				gg.SetCoordinateValue(manRaw + 1, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw + 1, manCol, 2);
+			}
+		}
+		gg.SetCoordinateValue(manRaw - 1, manCol, 5);
+		manRaw--;
+	}
+	public void backleft(int t, GameGrid gg){
+		if(t == 30){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+			}
+		}else if(t == 31){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 9);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 3);
+			}
+			if(ggtemp.getCoordinateValue(manRaw, manCol - 1) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol - 1) == 9){
+				gg.SetCoordinateValue(manRaw, manCol - 1, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol - 1, 2);
+			}
+		}
+		gg.SetCoordinateValue(manRaw, manCol + 1, 6);
+		manCol++;
+	}
+	public void backright(int t, GameGrid gg){
+		if(t == 40){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 2);
+			}
+		}else if(t == 41){
+			if(ggtemp.getCoordinateValue(manRaw, manCol) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol) == 9){
+				gg.SetCoordinateValue(manRaw, manCol, 9);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol, 3);
+			}
+			if(ggtemp.getCoordinateValue(manRaw, manCol + 1) == 4 ||
+				ggtemp.getCoordinateValue(manRaw, manCol + 1) == 9){
+				gg.SetCoordinateValue(manRaw, manCol + 1, 4);
+			}else{
+				gg.SetCoordinateValue(manRaw, manCol + 1, 2);
+			}
+		}
+		gg.SetCoordinateValue(manRaw, manCol - 1, 7);
+		manCol--;
+	}
+	
+	
+	public boolean isMystackEmpty(){
+		return mystack.isEmpty();
+	}
+	public int back(){
+		return mystack.pop();
+	}
+
 }
