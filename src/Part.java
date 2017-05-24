fa8e83d68823474e2f887ea8fa9aad1b13d1b21c
Zhizhen Chen <chenczz@163.com>
2017-05-24 18:00:49 +1000

    u can have a random(choose 1 from 1) puzzle now

--- src/Part.java
+++ src/Part.java
@@ -6,8 +6,6 @@ import java.util.Scanner;
 public class Part {
 	
 	private ArrayList<ArrayList<Integer>> p;
-	private Integer rowCount;
-	private Integer colCount;
 	private Integer boxCount;
 	
 	
@@ -20,23 +18,30 @@ public class Part {
 	
 	
 	public Part(int count){
+		p=new ArrayList<ArrayList<Integer>>();
+		
+		for(int i = 0; i < 9; i++){
+			this.p.add(new ArrayList<Integer>());
+			for(int j = 0; j < 9; j++){
+				this.p.get(i).add(0);
+			}
+		}
+		
+		
 		Scanner sc = null;
-		File f = new File("parts/" + count + ".txt");
+		File f = new File("levels/parts/" + count + ".txt");
 		boxCount=0;
 		
 		try{
 			sc = new Scanner(f);
-			if(sc.hasNextLine()){
-				String line = sc.nextLine();
-				for(String c: line.split("")){
-					int i = Integer.parseInt(c);
-					kindOfPart = i;
-				}
+			String line = sc.nextLine();
+			for(String c: line.split("")){
+				int i = Integer.parseInt(c);
+				kindOfPart = i;
 			}
-			while(sc.hasNextLine()){
-				String line = sc.nextLine();
-				this.addRow();
-				this.setRow(this.getRowCount(), this.StringToArrayList_Int(line));
+			for(int i = 0; i < 9; i++){
+				line = sc.nextLine();
+				this.setRow(i, this.StringToArrayList_Int(line));
 			}
 		}
 		catch(FileNotFoundException e){}
@@ -45,8 +50,8 @@ public class Part {
 	    	  if (sc != null) sc.close();
 	    }
 		
-		for(int i=0;i<rowCount;i++){
-			for(int j=0;j<colCount;j++){
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
 				if(p.get(i).get(j)==3){
 					boxCount++;
 				}
@@ -54,10 +59,8 @@ public class Part {
 		}
 	}
 	
-	private Part(ArrayList<ArrayList<Integer>> p, Integer rowCount,Integer colCount,Integer boxCount, Integer kindOfPart){
+	private Part(ArrayList<ArrayList<Integer>> p,Integer boxCount, Integer kindOfPart){
 		this.p=p;
-		this.rowCount=rowCount;
-		this.colCount=colCount;
 		this.boxCount=boxCount;
 		this.kindOfPart=kindOfPart;
 	}
@@ -67,27 +70,10 @@ public class Part {
 	}
 	
 	private void setRow(int r, ArrayList<Integer> input){
-		if(this.colCount == 0){
-			colCount = input.size();
+		this.p.get(r).clear();
+		for(int i = 0; i < 9; i++){
+			this.p.get(r).add(input.get(i));
 		}
-		
-		this.p.get(r - 1).clear();
-		for(int i = 0; i < colCount; i++){
-			this.p.get(r - 1).add(input.get(i));
-		}
-	}
-	
-	private void addRow(){
-		this.p.add(new ArrayList<Integer>());
-		this.rowCount++;
-	}
-	
-	public int getRowCount() {
-		return rowCount;
-	}
-	
-	public int getColCount() {
-		return colCount;
 	}
 	
 	/**
@@ -97,11 +83,22 @@ public class Part {
 	private ArrayList<Integer> StringToArrayList_Int(String s){
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		for(String c: s.split("")){
-			int i = Integer.parseInt(c);
-			l.add(i);
+			if(c!=null){
+				int i = Integer.parseInt(c);
+				l.add(i);
+			}
 		}
 		
 		return l;
+	}
+	
+	public void printPart(){
+		for(ArrayList<Integer> row: this.p){
+			for(Integer col: row){
+				System.out.print(col + "  ");
+			}
+			System.out.print("\n");
+		}
 	}
 	
 	public ArrayList<ArrayList<Integer>> getPart() {
@@ -112,32 +109,32 @@ public class Part {
 		ArrayList<ArrayList<Integer>> newP = new  ArrayList<ArrayList<Integer>>();
 		for (ArrayList<Integer> row:p){
 			ArrayList<Integer> newR = new ArrayList<Integer>();
-			for (int i=rowCount-1;i>=0;i--){
+			for (int i=8;i>=0;i--){
 				newR.add(row.get(i));
 			}
 			newP.add(newR);
 		}
-		return new Part(newP,rowCount,colCount,boxCount,kindOfPart);
+		return new Part(newP,boxCount,kindOfPart);
 	}
 	
 	public Part upToDown(){
 		ArrayList<ArrayList<Integer>> newP = new  ArrayList<ArrayList<Integer>>();
-		for (int i=colCount-1;i>=0;i--){
+		for (int i=8;i>=0;i--){
 			newP.add(p.get(i));
 		}
-		return new Part(newP,rowCount,colCount,boxCount,kindOfPart);
+		return new Part(newP,boxCount,kindOfPart);
 	}
 	
 	public Part iToj(){
 		ArrayList<ArrayList<Integer>> newP = new  ArrayList<ArrayList<Integer>>();
-		for (int j=rowCount-1;j>=0;j--){
+		for (int j=8;j>=0;j--){
 			ArrayList<Integer> newR = new ArrayList<Integer>();
-			for (int i=rowCount-1;i>=0;i--){
+			for (int i=8;i>=0;i--){
 				newR.add(p.get(i).get(j));
 			}
 			newP.add(newR);
 		}
-		return new Part(newP,rowCount,colCount,boxCount,kindOfPart);
+		return new Part(newP,boxCount,kindOfPart);
 	}
 	
 	public Integer getBoxCount(){
--- src/RandomLevel.java
+++ src/RandomLevel.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -6,16 +9,21 @@ public class RandomLevel {
 	private ArrayList<Part> starts;
 	private ArrayList<Part> passes;
 	private ArrayList<Part> objectives;
-	private final static Integer numberOfParts=3;
+	private final static Integer numberOfParts=4;
+	private Part p0;
 	private Part p11;
 	private Part p12;
 	private Part p21;
 	private Part p22;
 	
 	public RandomLevel(){
+		starts=new ArrayList<Part>();
+		passes=new ArrayList<Part>();
+		objectives=new ArrayList<Part>();
 		for(int i = 0;i<numberOfParts;i++){
 			Part p=new Part(i);
 			switch(p.getKind()){
+				case 0:p0=p;break;
 				case 1:starts.add(p);break;
 				case 2:passes.add(p);break;
 				case 3:objectives.add(p);break;
@@ -28,7 +36,101 @@ public class RandomLevel {
 		Random r = new Random();
 		p11 = starts.get(r.nextInt(starts.size()));
 		p12 = passes.get(r.nextInt(passes.size())).rightToLeft();
+		p21 = p0;
 		p22 = objectives.get(r.nextInt(objectives.size())).rightToLeft().upToDown();
 		
+		try {
+        	File file = new File("levels/random.wml");
+			PrintWriter writer = new PrintWriter(file);
+			
+			for(int i = 0; i < 21; i++){
+				if(i==0 || i==10 || i==20){
+					for(int j = 0; j < 21; j++){
+						if( i==10 &&((j==5 && p11.getPart().get(4).get(8)>1 && p21.getPart().get(4).get(0)>1) || j==15 && p12.getPart().get(4).get(8)>1 && p22.getPart().get(4).get(0)>1)){
+							writer.write(Integer.toString(2));
+						}else if(i==0 && ((j>0 && j<10 && p11.getPart().get(0).get(j-1)>1) || (j>10 && j<20 && p12.getPart().get(j-11).get(0)>1))){
+							writer.write(Integer.toString(1));
+						}else if(i==20 && ((j>0 && j<10 && p21.getPart().get(8).get(j-1)>1) || (j>10 && j<20 && p22.getPart().get(j-11).get(8)>1))){
+							writer.write(Integer.toString(1));
+						}else if(i== 10){
+							writer.write(Integer.toString(1));
+						}else{
+							writer.write(Integer.toString(0));
+						}
+					}
+				}else if(i<10){
+					if(p11.getPart().get(0).get(0)==0){
+						writer.write(Integer.toString(0));
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p11.getPart().get(i-1).get(j-1)));
+						}
+						writer.write(Integer.toString(1));
+					}else{
+						writer.write(Integer.toString(1));
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p11.getPart().get(i-1).get(j-1)));
+						}
+						if(i==5){
+							writer.write(Integer.toString(2));
+						}else{
+							writer.write(Integer.toString(1));
+						}
+					}
+					
+					if(p12.getPart().get(0).get(0)==0){
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p12.getPart().get(i-1).get(j-1)));
+						}
+						writer.write(Integer.toString(0));
+					}else{
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p12.getPart().get(i-1).get(j-1)));
+						}
+						writer.write(Integer.toString(1));
+					}
+				}else{
+					if(p21.getPart().get(0).get(0)==0){
+						writer.write(Integer.toString(0));
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p21.getPart().get(i-11).get(j-1)));
+						}
+						writer.write(Integer.toString(1));
+					}else{
+						writer.write(Integer.toString(1));
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p21.getPart().get(i-11).get(j-1)));
+						}
+						if(i==5){
+							writer.write(Integer.toString(2));
+						}else{
+							writer.write(Integer.toString(1));
+						}
+					}
+					
+					if(p22.getPart().get(0).get(0)==0){
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p22.getPart().get(i-11).get(j-1)));
+						}
+						writer.write(Integer.toString(0));
+					}else{
+						for(int j = 1; j < 10; j++){
+							writer.write(Integer.toString(p22.getPart().get(i-11).get(j-1)));
+						}
+						writer.write(Integer.toString(1));
+					}
+				}
+				writer.write("\n");
+			}
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Cannot Save");
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
+	
 }
