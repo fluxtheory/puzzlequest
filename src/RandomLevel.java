d4ed682312b40afa9ec5e61fe80cb5caa9842dd4
Zhizhen Chen <chenczz@163.com>
2017-05-24 16:09:55 +1000

    push the correct part

--- src/RandomLevel.java
+++ src/RandomLevel.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -32,34 +30,5 @@ public class RandomLevel {
 		p12 = passes.get(r.nextInt(passes.size())).rightToLeft();
 		p22 = objectives.get(r.nextInt(objectives.size())).rightToLeft().upToDown();
 		
-		
-=======
-import java.util.ArrayList;
-
-public class RandomLevel {
-	
-	private ArrayList<Part> starts;
-	private ArrayList<Part> passes;
-	private ArrayList<Part> objectives;
-	private final static Integer numberOfParts=5;
-	
-	public RandomLevel(){
-		for(int i = 0;i<numberOfParts;i++){
-			Part p=new Part(i);
-			switch(p.getKind()){
-				case 1:starts.add(p);break;
-				case 2:passes.add(p);break;
-				case 3:objectives.add(p);break;
-			}
-		}
 	}
-	
-	public GameGrid getRandomLevel(){
-		GameGrid gg = new GameGrid();
-		//todo random methods
-		return gg;
->>>>>>> branch 'master' of https://github.com/fluxtheory/puzzlequest.git
-	}
-	
-
 }
